84ae8009af65056274381f4506d991ac9ce998cd
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 11:11:15 +0530

    Area of circle code is completed

--- src/area/circle.java
+++ src/area/circle.java
@@ -3,7 +3,11 @@ package area;
 public class circle {
 	void area(int r)
 	{
-		
-	}
+	  int radius;
+	  double pi = 3.142, area;
+		     radius = 5;
+		      area = pi * radius * radius;
+		      System.out.println("Area of circle is :" + area);
+		    }
+		}
 
-}
